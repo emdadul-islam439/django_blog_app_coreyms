5d3e308da8a01700b1b3953e1b340769646a04ef
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-16 15:55:30 +0600

    added index.html and about.html files and connected them into the project

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>This is HOME page</h1>")
+    return render(request, "blog/index.html")
 
 def about(request):
-    return HttpResponse("<h1>This is ABOUT page</h1>")
+    return render(request, "blog/about.html")
