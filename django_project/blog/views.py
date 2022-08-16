159d3f7b00230c6ad435b28f61c2e988fd9f7a4a
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-16 17:08:16 +0600

    copy-pasted different code snippets from: https://github.com/CoreyMSchafer/code_snippets/tree/master/Django_Blog/03-Templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+
 # Create your views here.
 def home(request):
-    return render(request, "blog/index.html")
+    context = {
+        'posts': posts
+    }
+    return render(request, "blog/index.html", context)
 
 def about(request):
     return render(request, "blog/about.html", {"title" : "About"})
