60db7500074aad72d7b2f7a09472b064c11ee569
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-18 05:04:54 +0600

    made the form beautiful using crispy_forms

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -128,3 +129,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
