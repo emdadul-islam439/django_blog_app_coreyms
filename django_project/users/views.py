bb719696abe51ae0ae25f5cfd3af3c05c44af95c
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-18 18:30:00 +0600

    fixed 'new registered accounts are not saving in the db problem'

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -8,6 +8,7 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f"Your account has been created successfully! You can now login into your account.")
             return redirect('login')
