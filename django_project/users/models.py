69991cdda398e4a2a2074f474bf2cf3af1c0eee1
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-19 01:39:16 +0600

    fixed 'image not uploading' issue

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,6 +1,7 @@
 from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -9,3 +10,13 @@ class Profile(models.Model):
     
     def __str__(self) -> str:
         return f"{self.user.username} profile"
+    
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+        
+        img = Image.open(self.image.path)
+        
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
